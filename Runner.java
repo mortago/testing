7576b1416b71fa46020678961da1104a8eebef5d
arjavibahety <arjavibahety@gmail.com>
2019-12-17 12:07:05 +0800

    first commit

---
+++ Runner.java
@@ -0,0 +1,18 @@
+public class Runner {
+	public static void main(String[] args) {
+		DStack nums = new DStack();
+		nums.push(15);
+		nums.push(8);
+		nums.push(10);
+		nums.push(11);
+		nums.push(56);
+		nums.show();
+
+nums.pop();
+nums.pop();
+nums.pop();
+nums.pop();
+//nums.pop();
+nums.show();
+	}
+}
